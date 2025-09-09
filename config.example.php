c944adeec199166617d227334aa9d5961bdfe34b
Tomáš Bohata <tomas.bohata@ceskatelevize.cz>
2025-09-09 11:01:59 +0200

    Security: Move API keys to config.php and add .gitignore
    - Moved sensitive API keys from api.php to config.php
    - Added .gitignore to exclude config.php from version control
    - Created config.example.php as template
    - Updated api.php to load configuration from external file

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Tuya API Configuration Template
+ * Zkopírujte tento soubor jako config.php a vyplňte své údaje
+ */
+
+return [
+    'accessKey' => 'YOUR_ACCESS_KEY_HERE',
+    'secretKey' => 'YOUR_SECRET_KEY_HERE',
+    'projectCode' => 'YOUR_PROJECT_CODE_HERE',
+    'baseUrl' => 'https://openapi.tuyaeu.com'
+];
